fa982684746feb01244ce1307b793f5e8fd4dea9
Raghav Verma <130084894+Raghavcpp@users.noreply.github.com>
2025-10-01 08:35:00 +0530

    del

--- src/gsapUtils.js
+++
@@ -1,8 +0,0 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-export function ensureGsap() {
-  if (typeof window === "undefined") return gsap;
-  gsap.registerPlugin(ScrollTrigger);
-  return gsap;
-}
